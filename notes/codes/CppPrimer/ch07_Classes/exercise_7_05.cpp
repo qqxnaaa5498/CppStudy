5ebe293ae42e0cd2a564930cff490a3565e8c58e
qqxnaaa5498 <q9yao@eng.ucsd.edu>
2018-12-29 16:01:32 -0800

    Update exercise_7_05.cpp

--- notes/codes/CppPrimer/ch07_Classes/exercise_7_05.cpp
+++ notes/codes/CppPrimer/ch07_Classes/exercise_7_05.cpp
@@ -1,28 +1,24 @@
-/*
- * 练习7.5：在你的Person类中提供一些操作使其能够返回姓名和住址。这些函数是否
- * 应该是const的呢？解释原因。
- */
+//应该是const, 因为此处只需要调取常量对象的成员函数，而不用向对象中写入新值（不加const也不会报错）。
 
-// 应该是const的，因为这样就可以让常量对象调用此成员函数，否则会因为无法传入
-// 顶层const this指针导致无法调用。
-
-#include <iostream>
 #include <string>
+#include <iostream>
 
+using std::string;
+using std::cin;
 using std::cout;
 using std::endl;
-using std::cin;
 
-struct Person
-{
-	std::string GetName() const { return name; }
-	std::string GetAddr() const { return addr; }
-
-	std::string name;
-	std::string addr;
+struct Person {
+	string getName() const {return name;}
+	string getAddr() const {return addr;}
+	string name;
+	string addr;
 };
 
-int main()
-{
-	return 0;
+int main() {
+	Person andrew;
+	andrew.name = "andrew";
+	andrew.addr = "1101 Dexter N";
+	cout << andrew.getName() << ' ' << andrew.getAddr() << endl;
+
 }
